9783bf4a6986fc5c1394ae209890477b8c87fa45
MohsinAliSoomro <mohsinalisoomro23@gmail.com>
2020-06-20 09:46:09 +0500

    message added for exceed 10

--- src/App.js
+++ src/App.js
@@ -4,13 +4,21 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { Button } from 'reactstrap';
 function App() {
   let [count,setcount]=useState(0);
+  let [message,setmessage]=useState('');
   const countNum = () => {
-    setcount(count+1)
+    if(count >= 10){
+      setmessage('Number is greater than 10...')
+      setcount(0)
+    }
+    else {
+      setcount(count+1)
+    }
   }
   return (
     <div className="App">
       <h1 className="display-4">Counting App for users</h1>
         <h1>{count}</h1>
+        <h3>{message}</h3>
         <Button color="success" onClick={countNum} >click</Button>
         <p>Developed by : Mohsin Ali</p>
     </div>
